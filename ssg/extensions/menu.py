6404b4aecc04274bbf823203bb25967dc0fdd4a6
Hao Cheng <hao.cheng@sas.com>
2023-04-11 22:09:30 -0400

    menu extension

---
+++ ssg/extensions/menu.py
@@ -0,0 +1,22 @@
+from ssg import hooks, parsers
+
+files = []
+
+
+@hooks.register("collect_files")
+def collect_files(source, site_parsers):
+    valid = lambda(p): not isinstance(p, parsers.ResourceParser)
+    for path in source.rglob("*"):
+        for parser in list(filter(valid, site_parsers)):
+            if parser.valid_file_ext(path):
+                files.append(path)
+
+
+@hooks.register("generate_menu")
+def generate_menu(path, ext):
+    template = '<li><a href="{}{}">{}</a></li>'
+    menu_item = lambda(name, ext): template.format(name, ext, name.lower())
+    menu = menu_item(path.stem, ext)
+    "\n".join(menu)
+    return "<ul>\n{}<ul>\n{}".format(menu, html)
+
